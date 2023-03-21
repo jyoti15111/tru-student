2c3986d5e2789fde7333a3646de3939446a3ec09
jyoti15111 <128044910+jyoti15111@users.noreply.github.com>
2023-03-21 17:30:17 +0530

    fl

---
+++ tru-student.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Tru student
+Plugin URI: https://eventscalendartemplates.com/
+Description: Design awesome single event page for The Events Calendar free plugin (by modern tribe) using Event Page Templates Addon.
+Version: 1.1
+Author:  Cool Plugins
+Author URI: https://coolplugins.net/
+License:GPL2
+Text Domain:espbp
+*/
